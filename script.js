d42e8ea5ded97ca724abbcb5a8e602ffcea1e8cd
Lena <bespalaya-02@mail.ru>
2024-02-21 10:08:27 +0300

    correct

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ function addTask() {
 listContainer.addEventListener("click", function(e) {
     if(e.target.tagName ==="LI"){
         e.target.classList.toggle("checked");
-       // saveData();
+       saveData();
     }
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-       // saveData();
+       saveData();
     }
 },false);
 
